2f318740dafccc08a17d4685d1f61c25566d2e2f
EspaMC16 <markchristianespanol0516@gmail.com>
2025-05-07 15:22:25 +0800

    Fix file modal

--- IMS_app/static/IMS_app/js/modal.js
+++ IMS_app/static/IMS_app/js/modal.js
@@ -1,17 +1,20 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
+    const wrapper = document.getElementById("wrapper");
     const modal = document.getElementById("attendanceModal");
     const openModalBtn = document.getElementById("openModalBtn");
     const closeModalBtn = document.getElementById("closeModalBtn");
 
     // Open the modal
     openModalBtn.addEventListener("click", function() {
+      wrapper.style.display = "flex";
       modal.style.display = "flex";
       document.body.classList.add("no-scroll");
     });
 
     closeModalBtn.addEventListener("click", function() {
+      wrapper.style.display = "none";
       modal.style.display = "none";
       document.body.classList.remove("no-scroll");
     });
